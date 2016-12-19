5f0285805b4e71848452e9ec87f7825f91ff8c50
Yishen Miao <mys721tx@gmail.com>
2016-12-19 08:26:15 -0500

    Implementing a rudimentary reader
    Implements a rudimentary reader to read a file byte by byte.
    
    	new file:   sha256.c

---
+++ sha256.c
@@ -0,0 +1,57 @@
+/*
+ * sha256.c: an implementation of SHA-256 in C
+ *
+ * Copyright (c) 2016 Yishen Miao
+ *
+ * This file is part of sha256
+ *
+ * sha256 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * sha256 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sha256.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+
+	char t;
+
+	if (argc < 2) {
+		printf("Usage: %s [file]\n", argv[0]);
+		return 0;
+	}
+
+	fp = fopen(argv[1], "rb");
+
+	if (!fp) {
+		fprintf(
+			stderr,
+			"Unable to open %s: %s\n",
+			argv[1],
+			strerror(errno)
+		);
+
+		return errno;
+	}
+
+	while ((t = fgetc(fp)) != EOF) {
+		printf("0x%02x\n", t);
+	}
+
+	return 0;
+}
